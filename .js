4bc40ab796a41762368c18392353144ba0d5651e
Isaac Mondesir <isaacmondesir@gmail.com>
2023-01-26 23:48:15 -0500

    Initial Commit

---
+++ .js
@@ -0,0 +1,4 @@
+var name = "Phil";
+var luckyNumber = 8;
+console.log(name + "'s lucky number is " + luckyNumber);
+// "Phil's lucky number is 8"
---
+++ JavaScript/Push to GitHub/Button Clicker/script.js
@@ -0,0 +1,12 @@
+function hide(add) {
+    add.remove();
+}
+
+function turnOff(Login) {
+    Login.innerText = 
+    "Logout";
+}
+
+function alert(likes) {
+    alert
+}
